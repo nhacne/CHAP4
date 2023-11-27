1d70b6ae51c64782a7460e6c0ed15b59d4877f95
nhacne <142861451+nhacne@users.noreply.github.com>
2023-11-27 09:32:30 +0700

    Add files via upload

---
+++ Button.js
@@ -0,0 +1,19 @@
+function MyButton () {
+    function handleClick () {
+        alert('You clicked me !');
+    }
+    return (
+        <button onClick={handleClick}>
+            Click me
+        </button>
+    );
+}
+
+export default function MyApp () {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton />
+        </div>
+    );
+}
